d12095f001d442699f8908a2cd476b6e7d60b3b5
Xabier Gabiña <xabierland@gmail.com>
2025-02-23 14:28:34 +0100

    Añade un espacio en blanco para mejorar la legibilidad del código en main.js

--- public/js/main.js
+++ public/js/main.js
@@ -317,6 +317,7 @@ class Tienda
             document.getElementById('productName').placeholder = "Ej: Nombre del producto";
             document.getElementById('productPrice').placeholder = "Ej: 0.00";
             document.getElementById('productDescription').placeholder = "Describe el producto detalladamente...";
+            
             const tipo = document.getElementById('productType').value;
             const contenedorInputs = document.getElementById('optionalInput');
             contenedorInputs.innerHTML = '';
